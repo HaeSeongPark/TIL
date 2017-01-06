2885a08b2b36247f8c01a00c3522a0d051843048
cord7894 <cord7894@naver.com>
2017-01-06 14:04:29 +0900

    2017/01/06

--- javastudy/src/ch6/Ch6_6.java
+++ javastudy/src/ch6/Ch6_6.java
@@ -5,8 +5,16 @@ class MyMath6_6
 	long add(long a, long b){return a+b;}
 	long subtract(long a, long b){return a-b;}
 	long multiply(long a, long b){return a*b;}
-	double divide(double a, double b){return a/b;}
 	int remainder(int a, int b){return a%b;}
+	double divide(double a, double b)
+	{	//유효성검사
+		if(b==0)
+		{
+			System.out.println("0으로 나눌 수 없습니다.");
+			return 0;
+		}
+		return a/b;
+	}
 }
 
 public class Ch6_6 {
