c56a84d647569a9729bcb86e2d46d0cc695b7e9c
cord7894 <cord7894@naver.com>
2017-01-06 13:45:49 +0900

    2017/01/06

--- javastudy/src/ch6/Ch6_6.java
+++ javastudy/src/ch6/Ch6_6.java
@@ -1,8 +1,24 @@
 package ch6;
 
+class MyMath6_6
+{
+	long add(long a, long b){return a+b;}
+	long subtract(long a, long b){return a-b;}
+	long multiply(long a, long b){return a*b;}
+	double divide(double a, double b){return a/b;}
+}
+
 public class Ch6_6 {
 	public static void main(String[] args) {
+		MyMath6_6 mm = new MyMath6_6();
+		long addResult = mm.add(5L, 3L);
+		long subResult = mm.subtract(5L, 3L);
+		long mulResult = mm.multiply(5L, 3L);
+		double divResult = mm.divide(5L,3L);
 		
+		System.out.println("add(5L,3L)="+addResult);
+		System.out.println("sub(5L,3L)="+subResult);
+		System.out.println("mul(5L,3L)="+mulResult);
+		System.out.println("div(5L,3L)="+divResult);
 	}
-
 }
