519eb238bbca98b8f37bc758e55cdb7c10fd07f1
cord7894 <cord7894@gmail.com>
2017-01-06 20:31:49 +0900

    2017/01/06

--- javastudy/src/ch6/CallStackTest2.java
+++ javastudy/src/ch6/CallStackTest2.java
@@ -1,5 +1,18 @@
 package ch6;
 
 public class CallStackTest2 {
-
+	public static void main(String[] args) {
+		System.out.println("main(String[] args)이 시작됐음.");
+		firstMethod();
+		System.out.println("main(String[] args)이 끝났음.");
+	}
+	static void firstMethod(){
+		System.out.println("firstMethod() 시작");
+		secondMethod();
+		System.out.println("firstMethod() 끝");
+	}
+	static void secondMethod(){
+		System.out.println("secondMethod()시작");
+		System.out.println("secondMethod()끝");
+	}
 }
