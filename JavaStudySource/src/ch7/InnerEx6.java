728a7556599ab9249aa1799ab60de635584a2766
HaeSeongPark <cord7894@gmail.com>
2017-01-16 11:13:37 +0900

    2016/01/16 JavaInnerClass

---
+++ JavaStudySource/src/ch7/InnerEx6.java
@@ -0,0 +1,9 @@
+package ch7;
+
+public class InnerEx6 {
+	Object iv = new Object(){void method(){}};        //익명클래스
+	static Object cv = new Object(){void method(){}}; //익명클래스
+	void myMethod(){
+		Object lv = new Object(){void method(){}};    //익명클래스
+	}
+}
