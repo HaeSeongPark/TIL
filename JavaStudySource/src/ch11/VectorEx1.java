378f463b338daf5ee819febd10c5dd1456d4cbd5
HaeSeongPark <cord7894@gmail.com>
2017-01-29 23:28:56 +0900

    2017 / 01 / 29 Java Collection Framework

---
+++ JavaStudySource/src/ch11/VectorEx1.java
@@ -0,0 +1,38 @@
+package ch11;
+
+import java.util.*;
+
+public class VectorEx1 {
+
+	public static void main(String[] args) {
+		Vector v = new Vector(5);; // 용량(capacity)이 5인 Vector를 생성
+		v.add("1");
+		v.add("2");
+		v.add("3");
+		print(v);
+		
+		v.trimToSize();
+		System.out.println("=== After trimToSize() ===");
+		print(v);
+		
+		v.ensureCapacity(6);
+		System.out.println("=== After ensureCapacity(6) ===");
+		print(v);
+		
+		v.setSize(7);
+		System.out.println("=== After setSize(7) ===");
+		print(v);
+		
+		v.clear();
+		System.out.println("=== After clear() === ");
+		print(v);
+
+
+	}
+	static void print(Vector v)
+	{
+		System.out.println(v);
+		System.out.println("size :"+v.size());
+		System.out.println("capacity :"+v.capacity());
+	}
+}
