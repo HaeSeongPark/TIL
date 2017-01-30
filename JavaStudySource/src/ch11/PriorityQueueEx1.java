3e5d409941ad342b7af6daa1c2f7316fce462c09
HaeSeongPark <cord7894@gmail.com>
2017-01-30 23:16:10 +0900

    2017 / 01 / 30 / Collection Framework

---
+++ JavaStudySource/src/ch11/PriorityQueueEx1.java
@@ -0,0 +1,22 @@
+package ch11;
+
+import java.util.*;
+
+public class PriorityQueueEx1 {
+	public static void main(String[] args) {
+		Queue pq = new PriorityQueue();
+		pq.offer(3); // pq.offer(new Integer(3)); 오토박싱
+		pq.offer(1);
+		pq.offer(5);
+		pq.offer(2);
+		pq.offer(4);
+		System.out.println(pq);
+		
+		Object obj = null;
+		
+		//PriorityQueue에 저장된 요소를 하나씩 꺼낸다.
+		while((obj = pq.poll())!=null)
+			System.out.println(obj);
+		
+	}
+}
