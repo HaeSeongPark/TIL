81ac6206b0995f62ca7ba5bce71ff5a3d1277c02
HaeSeongPark <cord7894@gmail.com>
2017-03-02 23:24:47 +0900

    2017 / 03 / 02 Java IO

---
+++ JavaStudySource/src/ch15/FileEx7.java
@@ -0,0 +1,28 @@
+package ch15;
+
+import java.io.*;
+
+public class FileEx7 {
+	public static void main(String[] args) {
+		if(args.length != 1){
+			System.out.println("USAGE : java FileEx7 pattern");
+			System.exit(0);
+		}
+		
+		String currDir = System.getProperty("user.dir");
+		
+		File dir = new File(currDir);
+		final String pattern = args[0];
+		
+		//String[] list (FilenameFilter filter)
+		String[] files = dir.list(new FilenameFilter(){
+			public boolean accept(File dir, String name) {
+				return name.indexOf(pattern) != -1;
+			}
+		});
+		
+		for(int i=0; i<files.length; i++){
+			System.out.println(files[i]);
+		}
+	}
+}
