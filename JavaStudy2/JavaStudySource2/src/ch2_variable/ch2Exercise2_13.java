2658caa27a8483273a2eaa1f7bf70a486f8ddaed
HaeSeongPark <cord7894@gmail.com>
2017-03-09 13:24:44 +0900

    2017 / 03 / 09 Java Variable

---
+++ JavaStudy2/JavaStudySource2/src/ch2_variable/ch2Exercise2_13.java
@@ -0,0 +1,12 @@
+package ch2_variable;
+
+public class ch2Exercise2_13 {
+/*
+ * a. boolean : false
+ * b. char : '\u0000'
+ * c. float : 0.0 // float는 0.0f가 기본값. 
+ * d. int : 0
+ * e. long : 0 // long은 0L이 기본값
+ * f. String : "" // String은 참조형 타입. 모든 참조형 타입의 기본값은 null
+ */
+}
