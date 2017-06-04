a2b67b03f139afb5e7fa99e68d42e697d5f09bc0
HaeSeongPark <cord7894@gmail.com>
2017-06-04 21:45:57 +0900

    2017 / 06 / 04 TIL android layout

---
+++ android/TouchEvent/app/src/main/java/com/rhino/study/layout/GridLayoutActivity.java
@@ -0,0 +1,15 @@
+package com.rhino.study.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rhino.study.touchevent.R;
+
+public class GridLayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_layout);
+    }
+}
