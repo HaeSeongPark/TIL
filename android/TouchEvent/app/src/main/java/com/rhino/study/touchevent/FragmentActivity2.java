33f46990dfa09969a69af473c9cf10575441d5eb
HaeSeongPark <cord7894@gmail.com>
2017-06-04 13:15:19 +0900

    2017 / 06 / 04 android Fragment

--- android/TouchEvent/app/src/main/java/com/rhino/study/touchevent/FragmentActivity2.java
+++ android/TouchEvent/app/src/main/java/com/rhino/study/touchevent/FragmentActivity2.java
@@ -3,12 +3,14 @@ package com.rhino.study.touchevent;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.app.*;
 import android.os.Bundle;
 
 /*
 자바 코드에서 프래그멘트를 추가하는 방법
 
 가로 세로 모드 전환할 때 클릭하 한 게 남아있는데 v4 때문에 그러나
+nono land모드 따로 만들어야 됨!!! fragmentActivity5처럼
 v4는 api11이전 버전 지원할려는 거고
 그냥은 11이후버전용이니까
  */
@@ -19,10 +21,12 @@ public class FragmentActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        ArrayListFragment list = new ArrayListFragment();
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(android.R.id.content, list);
-        ft.commit();
+        if(savedInstanceState == null) {
+            ArrayListFragment list = new ArrayListFragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.add(android.R.id.content, list);
+            ft.commit();
+        }
     }
 }
--- android/TouchEvent/app/src/main/java/com/rhino/study/touchevent/TitlesFragment.java
+++ android/TouchEvent/app/src/main/java/com/rhino/study/touchevent/TitlesFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.ListFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
