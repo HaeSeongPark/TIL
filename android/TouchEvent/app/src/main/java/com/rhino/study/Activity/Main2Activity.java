00f2781a4a5e25b7305c62e54df17b4c3619d351
HaeSeongPark <cord7894@gmail.com>
2017-06-04 16:39:31 +0900

    2017 / 06 / 04 TIL android

---
+++ android/TouchEvent/app/src/main/java/com/rhino/study/Activity/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.rhino.study.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rhino.study.touchevent.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
