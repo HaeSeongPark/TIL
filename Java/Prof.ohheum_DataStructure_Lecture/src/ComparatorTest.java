2f65bb65ab71beafdcdfb0e82105579dbcc73ff2
HaeSeongPark <cord7894@gmail.com>
2017-06-09 16:13:50 +0900

    2017 / 06 / 09 TIL java comparator lambda

--- Java/Prof.ohheum_DataStructure_Lecture/src/ComparatorTest.java
+++ Java/Prof.ohheum_DataStructure_Lecture/src/ComparatorTest.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 // 람다식으로
 public class ComparatorTest
@@ -10,7 +11,8 @@ public class ComparatorTest
 		fruits[0] = new Fruit("apple",6);
 		fruits[1] = new Fruit("aa",7);
 //		Arrays.sort(fruits,(f1 ,f2) -> f1.name.compareTo(f2.name));
-		Arrays.sort(fruits,(f1,f2)->f2.quantity-f1.quantity);
+//		Arrays.sort(fruits,(f1,f2)->f2.quantity-f1.quantity);
+		Arrays.sort(fruits,Fruit.nameComparator);
 		
 		System.out.println(Arrays.toString(fruits));
 	}
@@ -30,5 +32,11 @@ class Fruit
 	{
 		return "Fruit [name=" + name + ", quantity=" + quantity + "]";
 	}
+	
+	public static Comparator<Fruit> nameComparator 
+					  		= (f1,f2)->f1.name.compareTo(f2.name);
+					
+	public static Comparator<Fruit> quantComparator
+							=(f1,f2)->f1.quantity - f2.quantity;
 	
 }
