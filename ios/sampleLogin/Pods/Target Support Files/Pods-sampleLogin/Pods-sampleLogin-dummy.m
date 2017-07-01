a0a3827e1a587733936ed7cdc3a4ef9055b70336
HaeSeongPark <cord7894@gmail.com>
2017-07-02 02:05:42 +0900

    2017 / 07 / 02 TIL ios facebookSampleLogin

---
+++ ios/sampleLogin/Pods/Target Support Files/Pods-sampleLogin/Pods-sampleLogin-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_sampleLogin : NSObject
+@end
+@implementation PodsDummy_Pods_sampleLogin
+@end
---
+++ ios/sampleLogin/sampleLogin/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  sampleLogin
+//
+//  Created by cord7894 on 2017. 7. 2..
+//  Copyright © 2017년 rhino. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let loginButton = FBSDKLoginButton()
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+        
+        loginButton.delegate = self
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil {
+            print(error)
+            return
+        }
+        print("Successfully logged in with facebook...")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
