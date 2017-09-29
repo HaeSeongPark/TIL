dacdc7b0d9d2564fd506684f632a185577499ff1
hellonature <hellonature@hellonatureui-MacBook-Pro.local>
2017-09-29 09:34:50 +0900

    2017.09.29

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -20,5 +20,45 @@
 // };
 
 // projects.display();
-var HTMLworkEmployer = '<a href="#">%data%';
-console.log(HTMLworkEmployer.replace("#","hello nature").replace("%data%",".net"));
+// var HTMLworkEmployer = '<a href="#">%data%';
+// console.log(HTMLworkEmployer.replace("#","hello nature").replace("%data%",".net"));
+
+// var s = "hello world!";
+// var word = s.substring(s.indexOf(" ")+1, s.length);
+// console.log(word);
+
+// var x = 1,
+// 	y = 2;
+// [x,y] = [y,x];
+// console.log(x+" "+y);
+
+// function test()
+// {
+// 	var i = 0;
+// 	if (true)
+// 	{
+// 		var j = 0;
+// 		for(var k=0; k<10; k++)
+// 		{
+// 			console.log("k"+k); //0~9
+// 		}
+// 		console.log(k);  // 10
+// 	}
+// 	console.log(j); // 0
+// }
+// test();
+
+// function factorial(x){
+// 	if (x<0) throw new Error("x must not be negative");
+// 	for(var f = 1; x>1; f*=x, x--){}
+// 	return f;
+// }
+// console.log(factorial(3));
+"use strict";
+var hasStrictMode = (function() {
+	return this===undefined
+}());
+console.log(hasStrictMode);
+
+var o = { x : 1 };
+console.log( o && o.x );
