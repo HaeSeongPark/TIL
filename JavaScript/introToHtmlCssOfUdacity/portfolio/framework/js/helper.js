f094b7f28b7b6b804cc7f72f3229223754164b0f
HaeSeongPark <cord7894@gmail.com>
2017-10-19 05:35:15 +0900

    modify portfolio source

--- JavaScript/introToHtmlCssOfUdacity/portfolio/framework/js/helper.js
+++ JavaScript/introToHtmlCssOfUdacity/portfolio/framework/js/helper.js
@@ -5,5 +5,17 @@ var HTMLfeaturedWorkTitle = '<h3>%data%</h3>';
 var HTMLfeaturedWorkLink = '<p><a href="%data%">Link to project</a></p>';
 
 
-var HTMlmodal = 
-'<div class="modal fade" id="project%colnum%" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title" id="myModalLabel">%title%</h4></div><div class="modal-body"><img class="img-responsive" src="%imgURL%"> %discription%<div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>';
+var HTMlmodal = '<div class="modal fade" id="project%colnum%" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">';
+		 HTMlmodal   +='<div class="modal-dialog">';
+		 HTMlmodal   +='<div class="modal-content">';
+		 HTMlmodal   +='<div class="modal-header">';
+		 HTMlmodal   +='<h4 class="modal-title" id="myModalLabel">';
+		 HTMlmodal   +='%title%';
+		 HTMlmodal   +='</h4>';
+		 HTMlmodal   +='</div>';
+		 HTMlmodal   +='<div class="modal-body">';
+		 HTMlmodal   +='<img class="img-responsive" src="%imgURL%">';
+		 HTMlmodal   +='%discription%';
+		 HTMlmodal   +='<div class="modal-footer">';
+		 HTMlmodal   +='<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>';
+		 HTMlmodal   +='</div></div></div></div>';
