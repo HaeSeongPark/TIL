a3431a8d52798bf6bbc51a1bff6dec4d8071018d
HaeSeongPark <cord7894@gmail.com>
2018-02-01 00:29:55 +0900

    add extra

--- ios/MyPlayground.playground/Pages/start.xcplaygroundpage/Contents.swift
+++ ios/MyPlayground.playground/Pages/start.xcplaygroundpage/Contents.swift
@@ -4,14 +4,20 @@
 
 //: 복습 : 변수(var), 상수(let), if(조건문), func(함수)
 
-//: 할 것 : func 추가(정의, 호출), 클래스, 프로그래밍, 프로그래밍언어, 주석의 의미, Casting(as), Optional( ?, ! ), Array(배열), if else == boolean
+//: 변수는 속성이고 대명사이다.  함수는 행위, 기능
 
+//: 할 것 : func 추가(정의, 호출), 클래스, 프로그래밍, 프로그래밍언어, 주석의 의미, Casting(as), Optional( ?, ! ), Array(배열), if else == boolean
 var test = "test"
 
-if test = "test"{
-    
-}
-
-var a = 1
-a = "abc"
+let thisCompayName = "헬로네이처"
+var worker = ["임진우", "박해성", "홍재진","전진수","진수연","윤재연"]
+worker.append("허광남")
+var workers = worker.joined(separator: " ")
+print("\(workers) work at a \(thisCompayName)")
+//if test = "test"{
+//
+//}
+//
+//var a = 1
+//a = "abc"
 //: [Next](@next)
