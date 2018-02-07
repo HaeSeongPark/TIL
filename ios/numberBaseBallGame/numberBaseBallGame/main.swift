5ae9f08378afc6ccfd4a1d1b2c5fdb0a8bffe5eb
HaeSeongPark <cord7894@gmail.com>
2018-02-07 13:39:52 +0900

    almost complete codesquad level2 task

--- ios/numberBaseBallGame/numberBaseBallGame/main.swift
+++ ios/numberBaseBallGame/numberBaseBallGame/main.swift
@@ -17,11 +17,6 @@
 import Foundation
 
 /**
- 입력값을 받는다.
- */
-
-
-/**
   유효성검사를 한다.
   문자열 마지막에 \n까지 포함해야 해서 4자리 수인지 아닌지 체크한다.
  */
@@ -74,7 +69,7 @@ func isAnswer() -> Bool{
 func makeRandomAnswer() -> [Int] {
     var randomAnswerArray = [Int]()
     while randomAnswerArray.count < 3 {
-        let randomNumber = arc4random_uniform(UInt32(10))
+        let randomNumber = arc4random_uniform(UInt32(9)) + 1
         
         if randomAnswerArray.contains(Int(randomNumber)) == false {
             randomAnswerArray.append(Int(randomNumber))
@@ -104,22 +99,53 @@ func makeInputIntArray(_ input:String) -> [Int] {
     return intArray
 }
 
-func compare(com:[Int], user:[Int]){
-    
+//TODO: 라인수 줄이거나 strike, ball 처리 함수로 나누기
+func compare(com:[Int], user:[Int]) ->(Int,Int){
+    var strikeCount = 0, ballCaount = 0
+    var tempBallArray = [Int]()
+    for i in 0..<com.count {
+        if (com[i] == user[i]){
+            strikeCount += 1
+        } else {
+            tempBallArray.append(user[i])
+        }
+    }
+    if tempBallArray.count > 0 {
+        for i in 0..<tempBallArray.count{
+            if com.contains(tempBallArray[i]) {
+                ballCaount += 1
+            }
+        }
+    }
+    return (strikeCount,ballCaount)
+}
+
+func printResult(_ strikeCount:Int, _ ballCount:Int){
+    if strikeCount != 0 {
+        print("\(strikeCount) 스트라이크 ", terminator:"")
+    }
+    if ballCount != 0 {
+        print("\(ballCount)볼")
+    }
 }
 
 func startNumberBsseBallGame(){
     // makeRandomAnswer, 정답은 바뀌면 안되니까 let으로 선언
     let randomAnswer = makeRandomAnswer()
     print("randomAnswer : \(randomAnswer)" )
-    // 입력을 받는다.
-    let inputArray =  makeInputIntArray(input())
-    print("inputArray : \(inputArray)")
     
-    compare(com: randomAnswer, user: inputArray)
-    //비교를 하고 결과 출력
-    // 정답이 아닐 때까지 반복
-    // 정답이면 종료
+    while true {
+        let inputArray =  makeInputIntArray(input())
+        print("inputArray : \(inputArray)")
+        
+        let (strikeCount, ballCount) = compare(com: randomAnswer, user: inputArray)
+        printResult(strikeCount, ballCount)
+        
+        if(strikeCount == 3) {
+            print("\n\(strikeCount)개의 숫자를 모두 맞히셨습니다! 게임종료")
+            break
+        }
+    }
 }
 
 startNumberBsseBallGame()
