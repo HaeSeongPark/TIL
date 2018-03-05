d4f34884a84a97882c0a15633b3865d4c11d989a
HaeSeongPark <cord7894@gmail.com>
2018-03-05 14:06:58 +0900

    init

---
+++ codeSquad/day1/UnitConverter/UnitConverter/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  UnitConverter
+//
+//  Created by rhino Q on 2018. 3. 5..
+//  Copyright © 2018년 rhino Q. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+var cm:String = "120cm"
+var numberOfCm:Int = cm.sb
