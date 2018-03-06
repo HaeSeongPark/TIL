12d8371ffb8348d2773986e0beadb04994a61c49
HaeSeongPark <cord7894@gmail.com>
2018-03-06 17:48:44 +0900

    피드백반영

--- codeSquad/unitConverter/unitConverter/main.swift
+++ codeSquad/unitConverter/unitConverter/main.swift
@@ -46,57 +46,72 @@ struct Length{
     func valueOfLengthUnit(lengthUnit:LengthUnit) -> String{
         return "\(lenghtCm * lengthUnit.ratio)\(lengthUnit.symbol)"
     }
-}
-
-func getUserInputValue() -> String?{
-    print("변환할 값과 단위를 입력해주세요 ex)180cm")
-        return readLine()
-}
-
-func startConvert(){
-    guard let inputValue = getUserInputValue() else {
-        print("유효하지 않은 값입니다")
-        return
-    }
-    let (lengthWithoutUnit,unit) = splitIntoLenghtAndUnit(inputValue: inputValue)
     
-    guard let resultUnit = Units[unit] else {
-        print("지원하지 않는 단위입니다.")
-        return
+    func printResult(_ result:Length, _ resultUnit:LengthUnit){
+        switch resultUnit {
+        case .cm:
+            print(self.valueOfLengthUnit(lengthUnit:.m))
+        case .m:
+            print(self.valueOfLengthUnit(lengthUnit: .cm))
+        }
+    }
+}
+
+
+
+
+
+struct RhinoUnitConverter{
+    func start(){
+        let userInput = self.getUserInputValue()
+        let (lengthWithoutUnit,unit) = self.splitIntoLenghtAndUnit(inputValue: userInput)
+        let resultUnit = Units[unit]! // 이미 invalidCheck를 했기 때문에 !
+        let result = Length(lengthWithoutUnit, lengthUnit: resultUnit)
+        result.printResult(result, resultUnit)
     }
     
-    let result = Length(lengthWithoutUnit, lengthUnit: resultUnit)
-    printResult(result, resultUnit)
-}
-
-func printResult(_ result:Length, _ resultUnit:LengthUnit){
-    switch resultUnit {
-    case .cm:
-        print(result.valueOfLengthUnit(lengthUnit:.m))
-    case .m:
-        print(result.valueOfLengthUnit(lengthUnit: .cm))
+    func getUserInputValue() -> String{
+        print("변환할 값과 단위를 입력해주세요 ex)180cm")
+        
+        var userInput:String = readLine()!
+        while self.invalidCheck(userInput) == false {
+            userInput = readLine()!
+        }
+        return userInput
     }
-}
-
-// TODO : 정규식으로 간단히 할 수 없는지 알아보기
-func splitIntoLenghtAndUnit(inputValue:String) -> (lenghtWithoutUnit:Double, unit:String){
-    // 숫자 셋 지정
-    let digitsAndDot:[Character] = ["0","1","2","3","4","5","6","7","8","9","."]
-    //문자열의 첫번 째 인 덱스
-    var tempIndex = inputValue.startIndex
-    // 숫자와 닷이 아닐 때까지 돈다
-    while digitsAndDot.contains(inputValue[tempIndex]) == true {
-        tempIndex = inputValue.index(after: tempIndex)
-    }
-    // 이미 위에서 확인 했으므로 !
-    let lenghtWithoutUnit = Double(inputValue[..<tempIndex].description)!
-    let unit:String = inputValue[tempIndex...].description
     
-    return (lenghtWithoutUnit,unit)
+    func invalidCheck(_ userInput:String) -> Bool{
+        guard userInput != "" else {
+            print("유효하지 않은 값입니다. 다시입력해주세요")
+            return false
+        }
+        
+        let (_ ,unit) = splitIntoLenghtAndUnit(inputValue: userInput)
+        
+        guard Units[unit] != nil else {
+            print("단위를 적지 않았거나 지원하지 않는 단위를 입력하셨습니다. 다시입력해주세요")
+            return false
+        }
+        
+        return true
+    }
+    //TODO: 정규식으로 간단히 할 수 없는지 알아보기
+    func splitIntoLenghtAndUnit(inputValue:String) -> (lenghtWithoutUnit:Double, unit:String){
+        // 숫자 셋 지정
+        let digitsAndDot:[Character] = ["0","1","2","3","4","5","6","7","8","9","."]
+        //문자열의 첫번 째 인 덱스
+        var tempIndex = inputValue.startIndex
+        // 숫자와 닷이 아닐 때까지 돈다
+        while tempIndex < inputValue.endIndex && digitsAndDot.contains(inputValue[tempIndex]) == true {
+            tempIndex = inputValue.index(after: tempIndex)
+        }
+        // 이미 위에서 확인 했으므로 !
+        let lenghtWithoutUnit = Double(inputValue[..<tempIndex].description)!
+        let unit:String = inputValue[tempIndex...].description
+        
+        return (lenghtWithoutUnit,unit)
+    }
 }
 
-startConvert()
-
-
-
-
+var rhinoUnitConverter = RhinoUnitConverter()
+rhinoUnitConverter.start()
