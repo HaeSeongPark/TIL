73f71a269919169ddea75e488710c0637505cac0
HaeSeongPark <cord7894@gmail.com>
2018-03-30 09:03:07 +0900

    1.음원재생기 - 2. 어플 만들기 - 3. 인터페이스 빌더의 객체를 코드와 연결(IBOutlet)

--- BoostCourse/MusicPlayer/MusicPlayer/ViewController.swift
+++ BoostCourse/MusicPlayer/MusicPlayer/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  MusicPlayer
 //
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var progressSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
