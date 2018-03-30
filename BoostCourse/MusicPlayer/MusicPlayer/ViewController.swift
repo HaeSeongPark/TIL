cb624975908d77db513f1571f1af45ee3de27aa4
HaeSeongPark <cord7894@gmail.com>
2018-03-30 21:36:24 +0900

    1.음원재생기 - 2. 어플 만들기 - 4. 인터페이스 빌더의 객체를 코드와 연결(IBAction)

--- BoostCourse/MusicPlayer/MusicPlayer/ViewController.swift
+++ BoostCourse/MusicPlayer/MusicPlayer/ViewController.swift
@@ -18,12 +18,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        print("Button tapped")
     }
-
-
-}
+    
+    @IBAction func slideValueChanged(_ sender: UISlider) {
+        print("slider value changed")
+    }
+    
+ }
 
