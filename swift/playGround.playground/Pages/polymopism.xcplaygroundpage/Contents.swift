f9e27b02cd7653786b1d6d0bb85460789db2ab5e
rhino <cord7894@gmail.com>
2018-04-04 17:56:36 +0900

    add simple polymopism code

--- swift/playGround.playground/Pages/polymopism.xcplaygroundpage/Contents.swift
+++ swift/playGround.playground/Pages/polymopism.xcplaygroundpage/Contents.swift
@@ -2,6 +2,48 @@
 
 import Foundation
 
-var str = "Hello, playground"
+
+protocol polymopismable{
+    func printSelf()
+}
+
+extension Double: polymopismable {
+    func printSelf(){
+        print("Double입니다.")
+    }
+}
+
+extension Int: polymopismable {
+    func printSelf(){
+        print("Int입니다.")
+    }
+
+}
+
+extension String: polymopismable {
+    func printSelf(){
+        print("strring입니다.")
+    }
+
+}
+
+func polymopism(_ value : polymopismable ) {
+    value.printSelf()
+}
+
+polymopism("1")
+
+//func polymopism(_ value : Any ) {
+//    if value is Double {
+//        print("double입니다.")
+//    } else if value is Int {
+//        print("Int입니다,")
+//    } else if value is String {
+//        print("String 입니다.")
+//    }
+//}
+
+
+
 
 //: [Next](@next)
