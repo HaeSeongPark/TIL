b06377e15ff45d4441792180512cbacebe3c65fd
rhino <cord7894@gmail.com>
2018-05-18 20:55:51 +0900

    before data model

---
+++ ios/startDevelopingiOSApp/startDevelopingiOSApp/RatingControl.swift
@@ -0,0 +1,130 @@
+//
+//  RatingControl.swift
+//  startDevelopingiOSApp
+//
+//  Created by rhino Q on 2018. 5. 18..
+//  Copyright © 2018년 rhino Q. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RatingControl: UIStackView {
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    @IBInspectable var rating:Int = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
+    
+    @IBInspectable var startSize:CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    @IBInspectable var startCount:Int = 5 {
+        didSet {
+            setupButtons()
+        }
+    }
+    
+    //MARK: Inittialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    //MARK: Private Methods
+    private func setupButtons() {
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        for index in 0..<startCount {
+            
+            let button = UIButton()
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
+            
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: startSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: startSize.width).isActive = true
+            
+            // Set the accessibility label
+            button.accessibilityLabel = "Set \(index+1) start rating"
+            
+            button.addTarget(self, action: #selector(ratingButtonTapped(button:)), for: .touchUpInside)
+            addArrangedSubview(button)
+            
+            ratingButtons.append(button)
+        }
+        
+        updateButtonSelectionStates()
+    }
+    
+    //MARK: Button Action
+    @objc func ratingButtonTapped(button: UIButton) {
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            rating = 0
+        } else {
+            rating = selectedRating
+        }
+    }
+    
+    private func updateButtonSelectionStates() {
+        for (index, button) in ratingButtons.enumerated() {
+            button.isSelected = index < rating
+            
+            let hintString: String?
+            if rating == index+1 {
+                hintString = "Tap to reset the rationg to zero."
+            } else {
+                hintString = nil
+            }
+            
+            let valueString: String
+            switch rating {
+            case 0:
+                valueString = "No rating set."
+            case 1:
+                valueString = "1 start set."
+            default:
+                valueString = "\(rating) starts set."
+            }
+            
+            button.accessibilityHint = hintString
+            button.accessibilityValue = valueString
+        }
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
