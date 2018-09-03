94f4ae1314bcd939b701d0b03bd999cb8d0ace2a
rhino <cord7894@gmail.com>
2018-09-03 12:09:13 +0900

    done

---
+++ macos/splitVC/splitVC/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  splitVC
+//
+//  Created by rhino Q on 03/09/2018.
+//  Copyright © 2018 rhino Q. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
