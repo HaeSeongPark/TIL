4087369892f3e8ef05f3b3237c60839605c5972a
rhino <cord7894@gmail.com>
2018-09-03 14:53:09 +0900

    error : this class is not key value coding-compliant for the key songs

---
+++ macos/ArrayController/ArrayController/Song.swift
@@ -0,0 +1,14 @@
+//
+//  Song.swift
+//  ArrayController
+//
+//  Created by rhino Q on 03/09/2018.
+//  Copyright © 2018 rhino Q. All rights reserved.
+//
+
+import Cocoa
+
+class Song: NSObject {
+    var name: String? = "New Song"
+    var bpm: Int = 100
+}
