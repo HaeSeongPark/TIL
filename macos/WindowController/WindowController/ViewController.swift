327476bcb8734239c52f713b75c65b3bea257c27
rhino <cord7894@gmail.com>
2018-09-04 12:38:54 +0900

    start window controller

---
+++ macos/WindowController/WindowController/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WindowController
+//
+//  Created by rhino Q on 04/09/2018.
+//  Copyright © 2018 rhino Q. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
