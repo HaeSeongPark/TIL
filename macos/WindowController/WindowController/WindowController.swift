e38f6d76f3a75ad6035f8c843000a1de010fb31e
rhino <cord7894@gmail.com>
2018-09-04 13:20:47 +0900

    add label in toolbar

---
+++ macos/WindowController/WindowController/WindowController.swift
@@ -0,0 +1,17 @@
+//
+//  WindowController.swift
+//  WindowController
+//
+//  Created by rhino Q on 04/09/2018.
+//  Copyright © 2018 rhino Q. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        window?.titleVisibility = .hidden
+    }
+    
+}
