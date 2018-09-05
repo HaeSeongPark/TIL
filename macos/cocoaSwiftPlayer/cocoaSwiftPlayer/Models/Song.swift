62638d359ad3ca639503c84075fbcf30df17c546
rhino <cord7894@gmail.com>
2018-09-05 17:20:09 +0900

    divide in 2 storyboard with main and song

---
+++ macos/cocoaSwiftPlayer/cocoaSwiftPlayer/Models/Song.swift
@@ -0,0 +1,13 @@
+//
+//  Song.swift
+//  cocoaSwiftPlayer
+//
+//  Created by rhino Q on 05/09/2018.
+//  Copyright © 2018 rhino Q. All rights reserved.
+//
+
+import Cocoa
+
+class Song: NSObject {
+    
+}
