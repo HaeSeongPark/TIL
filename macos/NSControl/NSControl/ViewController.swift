f3772dd1e7f0a4b8c0b1e0e1ac6bb6151d7d1848
rhino <cord7894@gmail.com>
2018-09-07 17:57:46 +0900

    nscombobox

--- macos/NSControl/NSControl/ViewController.swift
+++ macos/NSControl/NSControl/ViewController.swift
@@ -12,15 +12,47 @@ class ViewController: NSViewController {
 
     @IBOutlet weak var textField: NSTextField!
     @IBOutlet weak var genderControl: NSPopUpButton!
+    
+    @IBOutlet weak var countryCode: NSComboBox!
+    
+    var coutryCodes = ["HK +852", "China +85", "US +1"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //textfield
         textField.intValue = 30
+        
+        // gender
         genderControl.removeAllItems()
         genderControl.addItems(withTitles: ["Male", "Female"])
         
         let selectionIndex = genderControl.indexOfSelectedItem
         print("selected gender index : \(selectionIndex)")
         genderControl.selectItem(at: 1)
+        
+        //country Code
+        countryCode.removeAllItems()
+//        countryCode.addItems(withObjectValues: [1, "ABC", 3.5])
+        countryCode.usesDataSource = true
+        countryCode.dataSource = self
+        countryCode.delegate = self
+//        countryCode.reloadData()
     }
 }
 
+extension ViewController: NSComboBoxDataSource {
+    func numberOfItems(in comboBox: NSComboBox) -> Int {
+        return coutryCodes.count
+    }
+    
+    func comboBox(_ comboBox: NSComboBox, objectValueForItemAt index: Int) -> Any? {
+        return coutryCodes[index]
+    }
+}
+
+extension ViewController: NSComboBoxDelegate {
+    func comboBoxWillPopUp(_ notification: Notification) {
+        print("popup : \(countryCode.indexOfSelectedItem)")
+    }
+}
