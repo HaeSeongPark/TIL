25c266c9ae3271ec654e6a4f6938d9a65c099bb5
rhino <cord7894@gmail.com>
2018-09-07 10:58:20 +0900

    searching with search field, nspredicate, array controller

--- macos/ArrayController/ArrayController/ViewController.swift
+++ macos/ArrayController/ArrayController/ViewController.swift
@@ -9,7 +9,22 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet var arrayController: NSArrayController!
     
+    // Optimize Performance
+    lazy var predicate: NSPredicate = {
+        return NSPredicate(format: "(name contains[cd] $value) OR (bpm == $value.intValue)")
+    }()
+    
+    @IBAction func changeSearchQuery(_ sender: NSSearchField) {
+        if sender.stringValue.isEmpty {
+            arrayController.filterPredicate = nil
+        } else {
+//            arrayController.filterPredicate = NSPredicate(format: "(name contains[cd] %@) OR (bpm == %@.intValue)", sender.stringValue, sender.stringValue)
+            let dict = ["value": sender.stringValue]
+            arrayController.filterPredicate = predicate.withSubstitutionVariables(dict)
+        }
+    }
     @IBOutlet weak var tableView: NSTableView!
     
     override func viewDidLoad() {
