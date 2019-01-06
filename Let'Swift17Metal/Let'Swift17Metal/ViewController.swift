e25f3cdb257475c71f214def490679e162122558
rhino <cord7894@gmail.com>
2019-01-06 20:44:58 +0900

    modify comment

--- Let'Swift17Metal/Let'Swift17Metal/ViewController.swift
+++ Let'Swift17Metal/Let'Swift17Metal/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by rhino Q on 06/01/2019.
 //  Copyright © 2019 rhino Q. All rights reserved.
-// 물결 이후부터 해보기 TIL gitignore 작성
+// 물결 이후부터 해보기
 //  ~17:33, 단일 텍스처 ex 카메라 필터, 물결 (19:14), swimming pool (user interaction with touch)
 //  17:33 ~ 텍스처 + 텍스처 ex) 빗물표현(20:15)
 //          view ex) view에 직접 후처리(물결, 필터 등), 구이?(끈적이는...)
