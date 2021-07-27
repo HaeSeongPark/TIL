714a0c7a9903049c92bdd3d3faaa65f3a1987706
HaeSeongPark <cord7894@gmail.com>
2021-07-27 21:24:26 +0900

    Apply .gitignore

--- ios/sampleLogin/Pods/Target Support Files/Pods-sampleLogin/Pods-sampleLogin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_sampleLogin : NSObject
-@end
-@implementation PodsDummy_Pods_sampleLogin
-@end
--- macos/RxSwiftMac/Pods/Target Support Files/Pods-RxSwiftMac/Pods-RxSwiftMac-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxSwiftMac : NSObject
-@end
-@implementation PodsDummy_Pods_RxSwiftMac
-@end
